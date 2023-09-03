6ef4baab988c5405da7478c9a273ac477e09eb31
Jacobreg <132351466+Jacobreg@users.noreply.github.com>
2023-09-03 20:24:57 +0800

    Winton v1

--- menu.gd
+++ menu.gd
@@ -10,6 +10,15 @@ func togglemagicbuttons():
 	get_node("MagicMenuContainer/Defense").disabled = not get_node("MagicMenuContainer/Defense").disabled
 	get_node("MagicMenuContainer/MagicMenuBack").disabled = not get_node("MagicMenuContainer/MagicMenuBack").disabled
 
+func toggleaoebuttons():
+	get_node("OffenseMenu/Leap").disabled = not get_node("OffenseMenu/Leap").disabled
+	get_node("OffenseMenu/Tesla Shot").disabled = not get_node("OffenseMenu/Tesla Shot").disabled
+	get_node("OffenseMenu/Single Target").disabled = not get_node("OffenseMenu/Single Target").disabled
+
+func togglesinglebuttons():
+	get_node("OffenseMenu/Tesla Cannon").disabled = not get_node("OffenseMenu/Tesla Cannon").disabled
+	get_node("OffenseMenu/Primal Rage").disabled = not get_node("OffenseMenu/Primal Rage").disabled
+	get_node("OffenseMenu/AOE").disabled = not get_node("OffenseMenu/AOE").disabled
 
 func _on_attack_menu_button_pressed():
 	$AnimationPlayer.play("popup")
@@ -34,3 +43,64 @@ func _on_offense_pressed():
 func _on_offense_menu_back_pressed():
 	$AnimationPlayer.play_backwards("offensepopup")
 	togglemagicbuttons()
+
+func _on_axe_kick_toggled(button_pressed):
+	if button_pressed==true:
+		$AtkMenuContainer/Jab.disabled = true
+		$AtkMenuContainer/Straight.disabled = true
+		$AtkMenuContainer/Cross.disabled = true
+		$AtkMenuContainer/Grapple.disabled = true
+		$AtkMenuContainer/Confirm.disabled = false
+
+func _on_roundhouse_kick_toggled(button_pressed):
+	if button_pressed==true:
+		$AtkMenuContainer/Jab.disabled = true
+		$AtkMenuContainer/Straight.disabled = true
+		$AtkMenuContainer/Cross.disabled = true
+		$AtkMenuContainer/Grapple.disabled = true
+		$AtkMenuContainer/Confirm.disabled = false
+
+func _on_reset_pressed():
+	$AtkMenuContainer/Jab.disabled = false
+	$AtkMenuContainer/Straight.disabled = false
+	$AtkMenuContainer/Cross.disabled = false
+	$AtkMenuContainer/Grapple.disabled = false
+	$"AtkMenuContainer/Roundhouse Kick".disabled = false
+	$"AtkMenuContainer/Axe Kick".disabled = false
+	$AtkMenuContainer/Confirm.disabled = true
+
+
+func _on_straight_toggled(button_pressed):
+	$"AtkMenuContainer/Roundhouse Kick".disabled = true
+	$"AtkMenuContainer/Axe Kick".disabled = true
+	$AtkMenuContainer/Grapple.disabled = true
+	$AtkMenuContainer/Confirm.disabled = false
+
+func _on_jab_toggled(button_pressed):
+	$"AtkMenuContainer/Roundhouse Kick".disabled = true
+	$"AtkMenuContainer/Axe Kick".disabled = true
+	$AtkMenuContainer/Grapple.disabled = true
+	$AtkMenuContainer/Confirm.disabled = false
+
+func _on_cross_toggled(button_pressed):
+	$"AtkMenuContainer/Roundhouse Kick".disabled = true
+	$"AtkMenuContainer/Axe Kick".disabled = true
+	$AtkMenuContainer/Grapple.disabled = true
+	$AtkMenuContainer/Confirm.disabled = false
+	
+
+func _on_grapple_toggled(button_pressed):
+	$"AtkMenuContainer/Roundhouse Kick".disabled = true
+	$"AtkMenuContainer/Axe Kick".disabled = true
+	$AtkMenuContainer/Jab.disabled = false
+	$AtkMenuContainer/Straight.disabled = false
+	$AtkMenuContainer/Cross.disabled = false
+	$AtkMenuContainer/Confirm.disabled = false
+
+
+func _on_single_target_toggled(button_pressed):
+	togglesinglebuttons()
+
+
+func _on_aoe_toggled(button_pressed):
+	toggleaoebuttons()
